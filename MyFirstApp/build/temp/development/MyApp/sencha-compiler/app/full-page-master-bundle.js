f59080a5c0c2ea946c8a43fe14e6aab78a7e91b4
Bhavya TS <bhavya@inapp.com>
2016-02-15 12:47:44 +0530

    Initial commit

---
+++ MyFirstApp/build/temp/development/MyApp/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require D:\Sencha 6\MyFirstApp\app.js
---
+++ MyFirstApp/build/temp/production/MyApp/sass/config.rb
@@ -0,0 +1,2 @@
+Compass.add_project_configuration('..\..\..\..\..\sass\config.rb')
+cache_path = 'D:\Sencha 6\MyFirstApp/build/.sass-cache'
